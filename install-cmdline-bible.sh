ccaf1f3e5cb5ecf835b8dcb26cacd9b28c473529
Nizpew <144165519+nizpew@users.noreply.github.com>
2024-10-05 19:33:24 -0400

    autoinstaller upload

---
+++ install-cmdline-bible.sh
@@ -0,0 +1,34 @@
+cd
+
+git clone https://huggingface.co/rhasspy/piper-voices
+cd piper-voices
+
+
+
+#make binary
+echo -e '#!/bin/bash\ncd ~/Downloads/apps/piper/piper_linux_x86_64/piper\necho $@ | ./piper --model en_US-libritts-high.onnx --output-raw | aplay -r 22050 -f S16_LE -t raw -' | sudo tee /usr/local/bin/piper > /dev/null && sudo chmod +x /usr/local/bin/piper
+
+
+#clone speech synth repo
+git clone https://github.com/rhasspy/piper.git
+
+
+
+#dependencies
+sudo apt-get install -y eog bible-kjv espeak; mkdir ./Documents/constant-files; touch ./Documents/constant-files/teologylog
+cd piper
+make
+git clone https://huggingface.co/rhasspy/piper-voices
+mv piper-voices/en_US-libritts-high.onnx* ..
+
+cd
+echo "finished. you can run with piper 'text i want to speak'
+or 
+piper text i want to speak
+or even put subshells on it argument
+piper $(USER)
+
+"
+
+
+
