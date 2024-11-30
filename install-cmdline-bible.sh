b786a2176a8cba3c04ccc4693c5c9b0e0d4f352f
Nizpew <144165519+nizpew@users.noreply.github.com>
2024-11-29 22:54:41 -0400

    Update install-cmdline-bible.sh

--- install-cmdline-bible.sh
+++ install-cmdline-bible.sh
@@ -10,12 +10,15 @@ curl -sSL https://raw.githubusercontent.com/aandrew-me/tgpt/main/install | bash 
 
 
 #make binary
-echo -e '#!/bin/bash\ncd /home/$USER/piper/piper_linux_x86_64/piper\necho $@ | ./piper --model en_US-libritts-high.onnx --output-raw | aplay -r 22050 -f S16_LE -t raw -' | sudo tee /usr/local/bin/piper > /dev/null && sudo chmod +x /usr/local/bin/piper
+echo -e '#!/bin/bash\ncd /home/$USER/piper/piper\necho $@ | ./piper --model en_US-libritts-high.onnx --output-raw | aplay -r 22050 -f S16_LE -t raw -' | sudo tee /usr/local/bin/piper > /dev/null && sudo chmod +x /usr/local/bin/piper
 
 
 #clone speech synth repo
 cd
-git clone https://github.com/rhasspy/piper.git
+wget https://github.com/rhasspy/piper/releases/download/v1.2.0/piper_amd64.tar.gz
+tar -xvf piper_amd64.tar.gz
+
+##git clone https://github.com/rhasspy/piper.git
 
 
 
@@ -24,7 +27,6 @@ git clone https://github.com/rhasspy/piper.git
 
 sudo apt-get install -y eog bible-kjv espeak; mkdir /home/$USER/Documents/constant-files/; touch /home/$USER/Documents/constant-files/
 cd piper
-make
 git clone https://huggingface.co/rhasspy/piper-voices
 mv /home/$USER/piper-voices/en/en_US/libritts/high/en_US-libritts-high.onnx* /home/$USER
 
